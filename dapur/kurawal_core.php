3eba344caf14a30da245ff37539a633adc3c65ca
ianfleon <ianfleon08@gmail.com>
2021-09-13 02:02:08 +0900

    fungsi untuk looping folder

--- dapur/kurawal_core.php
+++ dapur/kurawal_core.php
@@ -11,8 +11,9 @@ if (isset($_POST['folder'])) {
     foreach ($my_dir as $my) {
         $item_path = $folder . '/' . $my;
         if (is_dir($item_path)) {
-            echo "DIR";
-            echo '<div class="file-item folder" onclick="_collapse_folder(this.id)" id="'.$my.'">'.$my.'</div>';
+            echo '<div class="file-item folder" onclick="_read_file(this.id)" id="'.$my.'">'.$my.'</div>';
+            $f = array_diff(scandir($item_path), ['.', '..']);
+            _Collapse_Folder($f);
         } else {
             echo '<div class="file-item" onclick="_read_file(this.id)" id="'.$my.'">'.$my.'</div>';
         }
@@ -25,5 +26,22 @@ if (isset($_POST['folder'])) {
 if (isset($_POST['file'])) {
     $file = $_POST['file'];
     echo file_get_contents($file);
-
 }
+
+function _Collapse_Folder($data) {
+    foreach($data as $d) {
+        echo $d;
+    }
+}
+
+// function _Loop_Folder($data) {
+//     foreach ($data as $d) {
+//         if (gettype($d) == 'array') {
+//             _Loop_Folder($d);
+//         } else {
+//             echo $d . "| ";
+//         }
+//     }
+// }
+
+// _Loop_Folder($data);
