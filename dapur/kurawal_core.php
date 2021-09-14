4f01163f38620fe99a7f400977d351389ee79baf
ianfleon <ianfleon08@gmail.com>
2021-09-14 15:32:40 +0900

    memperbaiki beberapa bug untuk ambil data file

--- dapur/kurawal_core.php
+++ dapur/kurawal_core.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 /* Handle Project Folder */
 if (isset($_POST['folder'])) {
     
@@ -9,14 +13,26 @@ if (isset($_POST['folder'])) {
     
     // Looping Data
     foreach ($my_dir as $my) {
-        $item_path = $folder . '/' . $my;
+
+        $item_path = $folder . $my;
+
         if (is_dir($item_path)) {
-            echo '<div class="file-item folder" onclick="_read_file(this.id)" id="'.$my.'">'.$my.'</div>';
-            $f = array_diff(scandir($item_path), ['.', '..']);
-            _Loop_Folder($item_path, $f);
-        } else {
-            echo '<div class="file-item" onclick="_read_file(this.id)" id="'.$my.'">'.$my.'</div>';
+            echo '<div class=" folder">'.$my.'</div>';
+            _Loop_Folder($item_path, array_diff(scandir($item_path), ['.', '..']));
         }
+
+        echo "<br/>";
+        
+    }
+
+    foreach ($my_dir as $my) {
+
+        $item_path = $folder . $my;
+
+        if (is_file($item_path)) {
+            echo '<div class="file-item" onclick="_read_file(this.id)" id="'.$item_path.'">'.$my.'</div>';
+        }
+
         echo "<br/>";
         
     }
@@ -25,17 +41,25 @@ if (isset($_POST['folder'])) {
 /* Handle File */
 if (isset($_POST['file'])) {
     $file = $_POST['file'];
-    echo file_get_contents($file);
+
+    if (pathinfo($file)['extension'] == 'html' || pathinfo($file)['extension'] == 'html' || pathinfo($file)['extension'] == 'xml') {
+        echo htmlentities(file_get_contents($file));
+    } else {
+        echo file_get_contents($file);
+    }
 }
 
 /* Looping Folder dalam Folder */
 function _Loop_Folder($path, $data) {
     foreach ($data as $d) {
+
+        $filelok = $path . "/" . $d;
+
         if (is_dir($path ."/". $d)) {
-            echo "Folder: " . $d . "<br/>";
-            _Loop_Folder($path, _CEK_DIR($path ."/". $d));
-        } else {
-            echo "File: " . $d .  "<br/>";
+            echo "📂 Folder: " . $d . "<br/>";
+            _Loop_Folder($path . "/" . $d, _CEK_DIR($path ."/". $d));
+        } elseif(is_file($path ."/". $d)) {
+            echo '<div class="file-item" onclick="_read_file(this.id)" id="'.$filelok.'">'.$d.'</div>';
         }
     }
 }
