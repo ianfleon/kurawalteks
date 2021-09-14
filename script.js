e2e14d924808d47ac0fd8b7b1e14784f694bf615
ianfleon <ianfleon08@gmail.com>
2021-09-14 20:44:02 +0900

    menambahkan fungsi save file dan perbaiki beberapa bug

--- script.js
+++ script.js
@@ -4,43 +4,56 @@ var uri = "E:/Reposaya/kurawalteks/fileku/";
 var sidebardir = document.getElementById('dir-item');
 
 /* Mengambil Data Direktori */
-function _POST(url, tipe, callback) {
+function _POST(url, tipe, file, callback) {
 
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "dapur/" + "kurawal_core.php", true);
+    xhr.open("POST", "dapur/" + file, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // header
 
-    //Send the proper header information along with the request
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    xhr.onreadystatechange = function () { // Call a function when the state changes.
+    xhr.onreadystatechange = function () {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            // Request finished. Do processing here.
-            // console.log(this.responseText);
             callback(this.responseText);
         }
     }
-
-    // console.log(url);
 
     xhr.send(tipe + "=" + encodeURI(url));
 
 }
 
 function _read_file(namafile) {
-    _POST(namafile, "file", function(data) {
-        console.log(document.getElementById('isi-content'));
-        document.getElementById('isi-content').innerHTML = data;
+    _POST(namafile, "file", "kurawal_core.php", function(data) {
+        const isi = document.getElementById('isi-content');
+        isi.setAttribute(['data-path'], namafile);
+        isi.value = data;
     });
 }
 
 function _collapse_folder(namafolder) {
-    _POST(uri + '/' + namafolder, "folder", function(items) {
-        // console.log(items);
+    _POST(uri + '/' + namafolder, "folder", "kurawal_core.php", function(items) {
         sidebardir.append(items);
     });
 }
 
-_POST(uri, "folder", function(data) {
-    // console.log(data);
+function _simpan_file() {
+
+    const konten = document.getElementById("isi-content");
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "dapur/simpan.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // header
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            console.log(this.responseText);
+        }
+    }
+
+    // console.log(konten.attributes["data-path"]);
+    // console.log(konten);
+
+    xhr.send("filepath=" + encodeURI(konten.attributes["data-path"].value) + "&" + "isifile=" + encodeURI(konten.value));
+
+}
+
+_POST(uri, "folder", "kurawal_core.php", function(data) {
     document.getElementById('dir-item').innerHTML = data;
 });
